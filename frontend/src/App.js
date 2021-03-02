53aad7621bd0a9c535278fc3113dc239fa599fcc
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-03-02 11:41:07 -0700

    added all container files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Containers/Header'
+import Body from './Containers/Body'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Body />
     </div>
   );
 }
