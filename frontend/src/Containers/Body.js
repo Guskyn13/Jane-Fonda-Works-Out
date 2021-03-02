ae7179ea0b54e8c0680bfca5aad64c72142769f7
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-03-02 11:53:28 -0700

    added component files and used a placeholder fetch call

--- frontend/src/Containers/Body.js
+++ frontend/src/Containers/Body.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ExercisesContainer from './ExercisesContainer'
+import FavoritesContainer from './FavoritesContainer'
 
 export default function Body() {
     return (
         <div>
-            
+            <FavoritesContainer />
+            <ExercisesContainer />
         </div>
     )
 }
